b9f3cc46f4f5c6f8c3003b53b194d00db1562882
Jacob North <56890989+jnorth314@users.noreply.github.com>
2024-10-05 17:57:54 -0500

    Fix pyinstaller build hidden import

--- tools/build_executable.bat
+++ tools/build_executable.bat
@@ -4,4 +4,5 @@ python3.12 -m PyInstaller %~dp0/../src/main.py^
     --windowed^
     --icon=%~dp0/../res/icon.ico^
     --add-data="%~dp0/../res;res"^
+    --hidden-import=comtypes.stream^
     --name=DarkDream
